3e1a81fc201da3cdd53c4ea762966615a644e81b
Charles <galc201093@gmail.com>
2017-12-09 14:32:31 +0100

    Charles: working model + combine prediction function (not tested yet)

--- project02/src/ourHelpers.py
+++ project02/src/ourHelpers.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import tensorflow as tf
 from helpers import *
 
 from sklearn.metrics import f1_score
@@ -45,3 +46,49 @@ def sk_mean_F1_score(prediction, groundtruth):
         y_pred = np.reshape(prediction[i], [-1])
         f1s.append(f1_score(y_true, y_pred, average='macro'))
     return np.array(f1s).mean()
+
+class EvalCheckpointSaverListener(tf.train.CheckpointSaverListener):
+    def __init__(self, estimator, input_fn):
+        self.estimator = estimator
+        self.input_fn = input_fn
+
+    def after_save(self, session, global_step):
+        print("after_save")
+        loss = self.estimator.evaluate(self.input_fn)
+        print("loss on test set" + str(loss))
+
+
+
+def combinePrediction(img, patch_size, stride, predict_fn):
+    """ apply per patch prediction to an image and combine the 
+    prediciton into one final image of same size as img
+
+    WARNING; the size of the image - the patch size must me dividable by the stride
+    Params:
+        img: numpy Array of shape[n,n]
+        patch_size: size of the patch given for prediction to predict_fn
+        stride: nb of pixel to be shifted between to patch
+        predict_fn: function that given a numpy array of 
+            shape[patch_size,patch_size] return a prediction of the same shape
+    
+    Returns:
+        An prediction for img with the same shape
+    """
+    assert len(img.shape) == 2, "The img must be an image and not a batch of images"
+    h = img.shape[0]
+    w = img.shape[1]
+    pred_final = np.zeros(img.shape) #Accumulator for the final prediction
+    pred_normalizer = np.zeros(img.shape) #Counter of the patch per prediction per pixel
+    assert h == w, "only squared image are accepted"
+    assert ((h - patch_size) % stride == 0), "The stride must be adaped on image and patch size"
+    nb_stride = (h - patch_size) / stride
+    for i in range(nb_stride):
+        for j in range(nb_stride): 
+            pred_final[i*stride: i*stride + patch_size,
+                     j*stride: j*stride + patch_size] += predict_fn(img[i*stride: i*stride + patch_size,
+                     j*stride: j*stride + patch_size])
+            pred_normalizer[i*stride: i*stride + patch_size,
+                     j*stride: j*stride + patch_size] += 1
+    return pred_final / pred_normalizer
+
+
